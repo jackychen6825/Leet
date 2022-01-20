fc45ae9ab4df8f52d3acf5de03078c8e989d25d4
jacky <jc6825@stern.nyu.edu>
2022-01-19 21:14:23 -0500

    less than count

--- leetcode.js
+++ leetcode.js
@@ -22,3 +22,45 @@ var kidsWithCandies = function(candies, extraCandies) {
     return res;
 };
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let goodPairs = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) goodPairs += 1
+        }
+    }
+    
+    return goodPairs;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+    let counts = {}, ans = [];
+    
+    for (let i = 0; i < nums.length; i++) {``
+        if (counts[nums[i]]) {
+            counts[nums[i]] += 1
+        } else {
+            counts[nums[i]] = 1
+        }
+    }
+    
+    for (let j = 0; j < nums.length; j++) {
+        let lessThanCount = 0;
+        Object.keys(counts).forEach(key => {
+            if (key < nums[j]) lessThanCount += counts[key]
+        })
+        ans.push(lessThanCount)
+    }
+    
+    return ans;
+};
+
