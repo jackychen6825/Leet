7c01e52f3250782afac5d5c26f7d82e646e0cc8d
jacky <jc6825@stern.nyu.edu>
2022-01-22 14:30:51 -0500

    2

--- leetcode.js
+++ leetcode.js
@@ -100,3 +100,18 @@ var middleNode = function(head) {
     return ans;
 };
 
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+};
