bed569b8fdefbbb12f7dba18cf8d63ed3147c713
jacky <jc6825@stern.nyu.edu>
2022-01-27 19:41:56 -0500

    1

--- leetcode.js
+++ leetcode.js
@@ -517,3 +517,22 @@ var findCenter = function(edges) {
     
     console.log(map)
 };
+
+/**
+ * @param {number[]} prices
+ * @return {number[]}
+ */
+var finalPrices = function(prices) {
+    //lets try the easy version first and then move on to stacks
+    
+    for (let i = 0; i < prices.length; i++) {
+        for (let j = i+1; j < prices.length; j++) {
+            if (prices[j] <= prices[i]) {
+                prices[i] = prices[i] - prices[j];
+                break;
+            }
+        }
+    }
+    
+    return prices;
+};
