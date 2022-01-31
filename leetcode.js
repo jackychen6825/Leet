31ff2e09f7b6f967db25728160a0735914ee4f71
jacky <jc6825@stern.nyu.edu>
2022-01-31 17:19:59 -0500

    leet

--- leetcode.js
+++ leetcode.js
@@ -818,3 +818,17 @@ var sortSentence = function(s) {
     
     return ans.join(' ');
 };
+
+var arrayStringsAreEqual = function(word1, word2) {
+    let str1 = "", 
+        str2 = "";
+    
+    let maxLength = Math.max(word1.length, word2.length);
+    
+    for (let i = 0; i < maxLength; i++) {
+        if (word1[i]) str1 += word1[i];
+        if (word2[i]) str2 += word2[i];
+    };
+    
+    return str1 === str2;
+};
