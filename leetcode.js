c92767e3ab4ec701fa247fc91468505ba7efcb19
jacky <jc6825@stern.nyu.edu>
2022-02-01 19:55:54 -0500

    leet

--- leetcode.js
+++ leetcode.js
@@ -1148,3 +1148,35 @@ BasicHashMap.prototype.delete = function(key) {
         this.values.splice(index, 1);
     };
 };
+
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * function ImmutableListNode() {
+ *    @ return {void}
+ *    this.printValue = function() { // print the value of this node.
+ *       ...
+ *    }; 
+ *
+ *    @return {ImmutableListNode}
+ *    this.getNext = function() { // return the next node.
+ *       ...
+ *    };
+ * };
+ */
+
+/**
+ * @param {ImmutableListNode} head
+ * @return {void}
+ */
+var printLinkedListInReverse = function(head) {
+    //recursive solution 
+    traverseNode(head);
+};
+
+var traverseNode = function(node) {
+    if (!node) return; //base case - what is the simplest?
+    const next = node.getNext();  //get the next node 
+    traverseNode(next);
+    node.printValue();
+};
