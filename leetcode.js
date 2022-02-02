da99e318196969188b41b7640fd8000c294a569c
jacky <jc6825@stern.nyu.edu>
2022-02-02 16:14:02 -0500

    sum of sub arr will either equal the next ele or the sum of the prev subarr and the next element

--- leetcode.js
+++ leetcode.js
@@ -1452,3 +1452,20 @@ var findSmallestSetOfVertices = function(n, edges) {
     
     return results;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    
+    let sum = nums[0];
+    let max = nums[0];
+    
+    for (let i = 1; i < nums.length; i++) {
+        sum = Math.max(nums[i], sum + nums[i]);
+        if (sum > max) max = sum;
+    };
+    
+    return max;
+};
