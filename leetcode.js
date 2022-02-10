dfaeb29e7111a29d1a79343ec4a717e3b904b80e
jacky <jc6825@stern.nyu.edu>
2022-02-10 12:38:29 -0500

    2

--- leetcode.js
+++ leetcode.js
@@ -2572,4 +2572,56 @@ MyQueue.prototype.empty = function() {
  * var param_2 = obj.pop()
  * var param_3 = obj.peek()
  * var param_4 = obj.empty()
+ * 
+ * 
+var MyStack = function() {
+    this.queue = [];
+    this.temp = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    while (this.queue.length > 1) {
+        const val = this.queue.shift();
+        this.temp.push(val);
+    };
+    
+    //queue only has one val left 
+    let ele = this.queue.shift();
+    this.queue = this.temp;
+    this.temp = []
+    return ele;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.queue[this.queue.length-1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.queue.length === 0
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
  */
