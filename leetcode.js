9e5efb6f7d117f353474b535d0e6498e7931b79b
jacky <jc6825@stern.nyu.edu>
2022-02-10 12:00:23 -0500

    to pop check length of stack 2 and if zero pop and push from stack 1 until empty then return last element

--- leetcode.js
+++ leetcode.js
@@ -2505,3 +2505,71 @@ var permute = function(nums) {
     return res;
 };
 
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+var numOfStrings = function(patterns, word) {
+    let count = 0;
+    
+    patterns.forEach(str => {
+        if (word.includes(str)) count++
+    })
+    
+    return count;
+};
+
+
+var MyQueue = function() {
+    this.stack1 = []
+    this.stack2 = []
+    this.front = null
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    if (this.stack1.length === 0) this.front = x;
+    this.stack1.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    //remove the first element from the front of the queue 
+    
+    if (this.stack2.length === 0) {
+        while (this.stack1.length !== 0) {
+            this.stack2.push(this.stack1.pop())
+        };
+    };
+    
+    return this.stack2.pop()
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.stack2.length === 0 ? this.front : this.stack2[this.stack2.length-1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return !this.stack1.length && !this.stack2.length
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
