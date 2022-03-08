608a6802f067f7de56ac0d80356c2fb7b038c8f6
jacky <jc6825@stern.nyu.edu>
2022-03-07 19:43:33 -0500

    two sum with sorted array and two pointers

--- leetcode.js
+++ leetcode.js
@@ -4918,3 +4918,29 @@ var twoSum = function(nums, target) {
     }
     
 };
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    
+    let left = 0,
+        right = numbers.length-1;
+    
+    while (left < right) {
+        let leftVal = numbers[left],
+            rightVal = numbers[right],
+            sum = leftVal + rightVal;
+        
+        if (sum === target) {
+            return [left+1, right+1]
+        } else if (sum > target) {
+            right--
+        } else {
+            left++
+        }
+    }
+    
+};
