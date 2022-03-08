b845a06de2eb5a62cd2b6715c88707c388030a90
jacky <jc6825@stern.nyu.edu>
2022-03-08 16:11:31 -0500

    two sum 2

--- leetcode.js
+++ leetcode.js
@@ -5009,3 +5009,27 @@ var moveZeroes = function(nums) {
     //iterate. through the array n times so linear time complexity 
     //we dont use any extra space so constant space complexity
 };
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    
+    let l = 0, 
+        r = numbers.length-1;
+    
+    while (l < r) {
+        let sum = numbers[l] + numbers[r];
+        
+        if (sum > target) {
+            r--;
+        } else if (sum < target) {
+            l++;
+        } else {
+            return [l+1, r+1]
+        }
+    }
+    
+};
