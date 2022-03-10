383951517cf1a255dc1d95eb724d83d4021ef0d7
jacky <jc6825@stern.nyu.edu>
2022-03-10 15:11:12 -0500

    two

--- leetcode.js
+++ leetcode.js
@@ -5383,3 +5383,33 @@ var maxProduct = function(nums) {
     
 };
 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    
+    let res = nums[0],
+        l = 0,
+        r = nums.length - 1,
+        m;
+    
+    while (l <= r) {
+        if (nums[l] < nums[r]) {
+            res = Math.min(res, nums[l])
+            break;
+        }
+        
+        m = Math.floor((l + r) / 2)
+        res = Math.min(res, nums[m])
+        
+        if (nums[m] >= nums[l]) {
+            l = m + 1
+        } else {
+            r = m - 1
+        }
+    }
+    
+    return res;
+    
+};
