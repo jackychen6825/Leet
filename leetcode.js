7368a00e23ecb207d60219825c8a72c39aba756e
jacky <jc6825@stern.nyu.edu>
2022-03-13 18:58:31 -0400

    l

--- leetcode.js
+++ leetcode.js
@@ -5548,3 +5548,30 @@ var groupAnagrams = function(strs) {
     return output;
     
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    
+    var helper = (p, q) => {
+        if (!p & !q) return true 
+        if (!p || !q) return false 
+        if (p.val !== q.val) return false 
+        
+        return helper(p.left, q.left) && helper(p.right, q.right)
+    }
+    
+    return helper(p, q)
+    
+};
