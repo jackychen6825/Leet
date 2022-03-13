ec4735457ac21767f933a77960b99e2a73e0e0b6
jacky <jc6825@stern.nyu.edu>
2022-03-13 19:22:42 -0400

    e

--- leetcode.js
+++ leetcode.js
@@ -5575,3 +5575,43 @@ var isSameTree = function(p, q) {
     return helper(p, q)
     
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    
+    if (!root) return [];
+    
+    let queue = [root],
+        output = [];
+    
+    while (queue.length) {
+        
+        let next = [],
+            curr = [];
+        
+        for (let n of queue) {
+            curr.push(n.val)
+            
+            if (n.left) next.push(n.left)
+            if (n.right) next.push(n.right)
+        }
+        
+        queue = next;
+        output.push(curr)
+        
+    }
+    
+    return output;
+    
+};
